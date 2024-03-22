e033d05c31cc390451e5c4558bbbd14c952d91a0
ian eddie <ianeddie2@gmail.com>
2024-03-22 12:37:00 -0300

    first commit

---
+++ src/lib/galery-data.ts
@@ -0,0 +1,24 @@
+export const data = [
+  {
+    image: 'https://i.postimg.cc/G2J98ZF3/4.jpg'
+  },
+  { image: 'https://i.postimg.cc/wMqMh02X/5.jpg' },
+  {
+    image: 'https://i.postimg.cc/SspJKyzt/1.jpg'
+  },
+  {
+    image: 'https://i.postimg.cc/kGT4wKYk/2.jpg'
+  },
+  {
+    image: 'https://i.postimg.cc/66cy9x6Y/3.jpg'
+  },
+  {
+    image: 'https://i.postimg.cc/pX08fQc6/33.jpg'
+  },
+  {
+    image: 'https://i.postimg.cc/jSm79xb6/22.jpg'
+  },
+  {
+    image: 'https://i.postimg.cc/pLpjq9gX/11.jpg'
+  }
+]
